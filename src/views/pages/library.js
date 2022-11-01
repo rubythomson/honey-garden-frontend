faf4b267f8a29465eb9704060e4c99cb09e1dcf7
“rubythomsongit config --global user.email ”Ruby.Thomson@students.curtin.edu.au <Ruby.Thomson@student.curtin.edu.au>
2022-11-01 15:03:21 +0800

    changes to library view

--- src/views/pages/library.js
+++ src/views/pages/library.js
@@ -51,65 +51,125 @@ class LibraryView {
           </div>
         </div>
 
-        <div class="library-archived-container">
+        <div class="library-current-reads-container">
           <div>
+            <div class="current-reads" style="margin-left: 12px;">
+              <progress max="100" value="70" class="progress-bar"></progress>
+            </div>
             <img src="/images/time-traveller.png" alt="time-traveller-img" class="serve-me-img" style="box-shadow: 0 0 10px lightgray;">
           </div>
           <div>
+            <div class="current-reads">
+              <progress max="100" value="70" class="progress-bar"></progress>
+            </div>
             <img src="/images/sing-song.png" alt="sing-song-img" class="serve-me-img" style="box-shadow: 0 0 10px lightgray;">
           </div>
           <div>
+            <div class="current-reads">
+              <progress max="100" value="50" class="progress-bar"></progress>
+            </div>
             <img src="/images/non-stop-babbling.png" alt="non-stop-babbling-img" class="serve-me-img" style="box-shadow: 0 0 10px lightgray;">
           </div>
           <div>
+            <div class="current-reads">
+              <progress max="100" value="70" class="progress-bar"></progress>
+            </div>
             <img src="/images/consistency.png" alt="consistency-img" class="serve-me-img" style="box-shadow: 0 0 10px lightgray;">
           </div>
           <div>
+            <div class="current-reads">
+              <progress max="100" value="60" class="progress-bar"></progress>
+            </div>
             <img src="/images/light-bulb.png" alt="light-bulb-img" class="serve-me-img" style="box-shadow: 0 0 10px lightgray;">
           </div>
           <div>
+            <div class="current-reads">
+              <progress max="100" value="0" class="progress-bar"></progress>
+            </div>
             <img src="/images/sparks.png" alt="sparks-img" class="serve-me-img" style="box-shadow: 0 0 10px lightgray;">
           </div>
           <div>
+            <div class="current-reads">
+              <progress max="100" value="0" class="progress-bar"></progress>
+            </div>
             <img src="/images/peaking-point.png" alt="peaking-point-img" class="serve-me-img" style="box-shadow: 0 0 10px lightgray;">
           </div>
           <div>
+            <div class="current-reads">
+              <progress max="100" value="40" class="progress-bar"></progress>
+            </div>
             <img src="/images/chess.png" alt="chess-img" class="serve-me-img" style="box-shadow: 0 0 10px lightgray;">
           </div>
           <div>
+            <div class="current-reads">
+              <progress max="100" value="0" class="progress-bar"></progress>
+            </div>
             <img src="/images/on-the-path.png" alt="on-the-path-img" class="serve-me-img" style="box-shadow: 0 0 10px lightgray;">
           </div>
           <div>
+            <div class="current-reads">
+              <progress max="100" value="0" class="progress-bar"></progress>
+            </div>
             <img src="/images/swan-castle.png" alt="swan-castle-img" class="serve-me-img" style="box-shadow: 0 0 10px lightgray;">
           </div>
           <div>
+            <div class="current-reads">
+              <progress max="100" value="0" class="progress-bar"></progress>
+            </div>
             <img src="/images/king.png" alt="king-img" class="serve-me-img" style="box-shadow: 0 0 10px lightgray;">
           </div>
           <div>
+            <div class="current-reads">
+              <progress max="100" value="0" class="progress-bar"></progress>
+            </div>
             <img src="/images/home.png" alt="home-img" class="serve-me-img" style="box-shadow: 0 0 10px lightgray;">
           </div>
           <div>
+            <div class="current-reads">
+              <progress max="100" value="80" class="progress-bar"></progress>
+            </div>
             <img src="/images/ruins.png" alt="ruins-img" class="serve-me-img" style="box-shadow: 0 0 10px lightgray;">
           </div>
           <div>
+            <div class="current-reads">
+              <progress max="100" value="70" class="progress-bar"></progress>
+            </div>
             <img src="/images/dark-side.png" alt="dark-side-img" class="serve-me-img" style="box-shadow: 0 0 10px lightgray;">
           </div>
           <div>
+            <div class="current-reads">
+              <progress max="100" value="0" class="progress-bar"></progress>
+            </div>
             <img src="/images/fairy-circle.png" alt="fairy-circle-img" class="serve-me-img" style="box-shadow: 0 0 10px lightgray;">
           </div>
           <div>
+            <div class="current-reads">
+              <progress max="100" value="30" class="progress-bar"></progress>
+            </div>
             <img src="/images/tipping-point.png" alt="tipping-point-img" class="serve-me-img" style="box-shadow: 0 0 10px lightgray;">
           </div>
           <div>
+            <div class="current-reads">
+              <progress max="100" value="50" class="progress-bar"></progress>
+            </div>
             <img src="/images/midnight.png" alt="midnight-img" class="serve-me-img" style="box-shadow: 0 0 10px lightgray;">
           </div>
           <div>
+            <div class="current-reads">
+              <progress max="100" value="20" class="progress-bar"></progress>
+            </div>
             <img src="/images/blooming.png" alt="blooming-img" class="serve-me-img" style="box-shadow: 0 0 10px lightgray;">
           </div>
           <div>
+            <div class="current-reads">
+              <progress max="100" value="0" class="progress-bar"></progress>
+            </div>
             <img src="/images/marry-me.png" alt="marry-me-img" class="serve-me-img" style="box-shadow: 0 0 10px lightgray;">
           </div>
           <div>
+            <div class="current-reads">
+              <progress max="100" value="70" class="progress-bar"></progress>
+            </div>
             <img src="/images/you-may-kiss-the-bride.png" alt="you-may-kiss-the-bride-img" class="serve-me-img" style="box-shadow: 0 0 10px lightgray;">
           </div>
         </div>
